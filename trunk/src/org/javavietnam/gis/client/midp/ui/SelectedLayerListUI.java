70c04560e6d5aab5f4ee53f2b7305ca8709fa924
khanh.lnq <khanh.lnq@0cb8caaa-af32-0410-8db2-b50025d4c983>
2007-12-28 05:46:57 +0000

    Review codes

--- trunk/src/org/javavietnam/gis/client/midp/ui/SelectedLayerListUI.java
+++ trunk/src/org/javavietnam/gis/client/midp/ui/SelectedLayerListUI.java
@@ -17,136 +17,145 @@ import javax.microedition.lcdui.List;
  * @author anntv
  * 
  */
+// FIXME VanAn: Refactor "SelectedLayerListUI" class to "SortLayerListUI"
 public class SelectedLayerListUI extends List implements CommandListener {
 
-	private final UIController uiController;
+    private final UIController uiController;
+    private final Command moveUpCommand;
+    private final Command moveDownCommand;
+    private final Command moveTopCommand;
+    private final Command moveBottomCommand;
+    private final Command getMapCommand;
+    private Vector selectedLayerList;
 
-	private final Command moveUpCommand;
+    public SelectedLayerListUI(UIController uiController) {
+        // TODO VanAn: Add ticker "Mark a layer and select moving action. NOTE: Moving action is not performed on highlighted layer" to SelectedLayerListUI
+        super(uiController.getString(UIConstants.LAYER_SORT), Choice.EXCLUSIVE);
 
-	private final Command moveDownCommand;
+        this.uiController = uiController;
+        // FIXME VanAn: This will only happen at the first time.
+        // Should use an init() method
+        setSelectedLayerList(uiController.getSelectedLayerList());
 
-	private final Command moveTopCommand;
+        // FIXME VanAn: Remove this
+        display(selectedLayerList);
 
-	private final Command moveBottomCommand;
+        getMapCommand = new Command(uiController.getString(UIConstants.GETMAP),
+            Command.SCREEN, 0);
+        // FIXME VanAn: Menu should be follow this order:
+        //    "Move to top"
+        //    "Move up"
+        //    "Move down"
+        //    "Move to bottom"
+        moveUpCommand = new Command(
+            uiController.getString(UIConstants.MOVE_UP), Command.SCREEN, 1);
+        moveDownCommand = new Command(uiController.getString(UIConstants.MOVE_DOWN), Command.SCREEN, 2);
+        moveTopCommand = new Command(uiController.getString(UIConstants.MOVE_TOP), Command.SCREEN, 3);
+        moveBottomCommand = new Command(uiController.getString(UIConstants.MOVE_BOTTOM), Command.SCREEN, 4);
 
-	private final Command getMapCommand;
+        addCommand(getMapCommand);
+        addCommand(moveUpCommand);
+        addCommand(moveDownCommand);
+        addCommand(moveTopCommand);
+        addCommand(moveBottomCommand);
 
-	private Vector selectedLayerList;
+        setCommandListener(this);
+    }
 
-	public SelectedLayerListUI(UIController uiController) {
-		super(uiController.getString(UIConstants.LAYER_SORT), Choice.EXCLUSIVE);
+    // FIXME VanAn: Should not re-draw the whole list. Remove this method.
+    public void display(Vector selectedLayerList) {
+        deleteAll();
+        for (int i = 0; i < selectedLayerList.size(); i++) {
+            append(selectedLayerList.elementAt(i).toString(), null);
+        }
+    }
 
-		this.uiController = uiController;
-		setSelectedLayerList(uiController.getSelectedLayerList());
+    /**
+     * @return the selectedLayerList
+     */
+    public Vector getSelectedLayerList() {
+        return selectedLayerList;
+    }
 
-		display(selectedLayerList);
+    /**
+     * @param selectedLayerList
+     *            the selectedLayerList to set
+     */
+    // FIXME VanAn: Should change to an init() method
+    public void setSelectedLayerList(Vector selectedLayerList) {
+        this.selectedLayerList = selectedLayerList;
+    // FIXME VanAn: Delete all and re-initialize the List items here
+    }
 
-		getMapCommand = new Command(uiController.getString(UIConstants.GETMAP),
-				Command.SCREEN, 0);
-		moveUpCommand = new Command(
-				uiController.getString(UIConstants.MOVE_UP), Command.SCREEN, 1);
-		moveDownCommand = new Command(uiController
-				.getString(UIConstants.MOVE_DOWN), Command.SCREEN, 2);
-		moveTopCommand = new Command(uiController
-				.getString(UIConstants.MOVE_TOP), Command.SCREEN, 3);
-		moveBottomCommand = new Command(uiController
-				.getString(UIConstants.MOVE_BOTTOM), Command.SCREEN, 4);
+    public void moveUp() {
+        // FIXME VanAn: The list should be circled
+        if (getSelectedIndex() > 0) {
+            int selectedIndex = getSelectedIndex();
+            Object selectedItem = selectedLayerList.elementAt(selectedIndex);
+            selectedLayerList.removeElementAt(selectedIndex);
+            selectedLayerList.insertElementAt(selectedItem, selectedIndex - 1);
 
-		addCommand(getMapCommand);
-		addCommand(moveUpCommand);
-		addCommand(moveDownCommand);
-		addCommand(moveTopCommand);
-		addCommand(moveBottomCommand);
+            // FIXME VanAn: Use List.delete, List.insert instead of re-draw the whole List
+            display(selectedLayerList);
+            setSelectedIndex(selectedIndex - 1, true);
+        }
+    }
 
-		setCommandListener(this);
-	}
+    public void moveDown() {
+        if (getSelectedIndex() < selectedLayerList.size() - 1) {
+            int selectedIndex = getSelectedIndex();
 
-	public void display(Vector selectedLayerList) {
-		deleteAll();
-		for (int i = 0; i < selectedLayerList.size(); i++) {
-			append(selectedLayerList.elementAt(i).toString(), null);
-		}
-	}
+            Object selectedItem = selectedLayerList.elementAt(selectedIndex);
+            selectedLayerList.removeElementAt(selectedIndex);
+            selectedLayerList.insertElementAt(selectedItem, selectedIndex + 1);
 
-	/**
-	 * @return the selectedLayerList
-	 */
-	public Vector getSelectedLayerList() {
-		return selectedLayerList;
-	}
+            // FIXME VanAn: Use List.delete, List.insert instead of re-draw the whole List
+            display(selectedLayerList);
+            setSelectedIndex(selectedIndex + 1, true);
+        }
+    }
 
-	/**
-	 * @param selectedLayerList
-	 *            the selectedLayerList to set
-	 */
-	public void setSelectedLayerList(Vector selectedLayerList) {
-		this.selectedLayerList = selectedLayerList;
-	}
+    public void moveTop() {
+        if (getSelectedIndex() > 0) {
+            int selectedIndex = getSelectedIndex();
 
-	public void moveUp() {
-		if (getSelectedIndex() > 0) {
-			int selectedIndex = getSelectedIndex();
-			Object selectedItem = selectedLayerList.elementAt(selectedIndex);
-			selectedLayerList.removeElementAt(selectedIndex);
-			selectedLayerList.insertElementAt(selectedItem, selectedIndex - 1);
-			display(selectedLayerList);
-			setSelectedIndex(selectedIndex - 1, true);
-		}
-	}
+            Object selectedItem = selectedLayerList.elementAt(selectedIndex);
+            selectedLayerList.removeElementAt(selectedIndex);
+            selectedLayerList.insertElementAt(selectedItem, 0);
 
-	public void moveDown() {
-		if (getSelectedIndex() < selectedLayerList.size()-1) {
-			int selectedIndex = getSelectedIndex();
+            // FIXME VanAn: Use List.delete, List.insert instead of re-draw the whole List
+            display(selectedLayerList);
+            setSelectedIndex(0, true);
+        }
+    }
 
-			Object selectedItem = selectedLayerList.elementAt(selectedIndex);
-			selectedLayerList.removeElementAt(selectedIndex);
-			selectedLayerList.insertElementAt(selectedItem, selectedIndex + 1);
+    public void moveBottom() {
+        if (getSelectedIndex() < selectedLayerList.size() - 1) {
+            int selectedIndex = getSelectedIndex();
 
-			display(selectedLayerList);
-			setSelectedIndex(selectedIndex + 1, true);
-		}
-	}
+            Object selectedItem = selectedLayerList.elementAt(selectedIndex);
+            selectedLayerList.removeElementAt(selectedIndex);
+            selectedLayerList.insertElementAt(selectedItem, selectedLayerList.size());
 
-	public void moveTop() {
-		if (getSelectedIndex() > 0) {
-			int selectedIndex = getSelectedIndex();
+            // FIXME VanAn: Use List.delete, List.insert instead of re-draw the whole List
+            display(selectedLayerList);
+            setSelectedIndex(selectedLayerList.size() - 1, true);
+        }
+    }
 
-			Object selectedItem = selectedLayerList.elementAt(selectedIndex);
-			selectedLayerList.removeElementAt(selectedIndex);
-			selectedLayerList.insertElementAt(selectedItem, 0);
-
-			display(selectedLayerList);
-			setSelectedIndex(0, true);
-		}
-	}
-
-	public void moveBottom() {
-		if (getSelectedIndex() < selectedLayerList.size()-1) {
-			int selectedIndex = getSelectedIndex();
-
-			Object selectedItem = selectedLayerList.elementAt(selectedIndex);
-			selectedLayerList.removeElementAt(selectedIndex);
-			selectedLayerList.insertElementAt(selectedItem, selectedLayerList
-					.size());
-
-			display(selectedLayerList);
-			setSelectedIndex(selectedLayerList.size() - 1, true);
-		}
-	}
-
-	public void commandAction(Command command, Displayable displayable) {
-		if (command == getMapCommand) {
-			uiController.getMapRequested();
-		} else if (command == moveUpCommand) {
-			moveUp();
-		} else if (command == moveDownCommand) {
-			moveDown();
-		} else if (command == moveTopCommand) {
-			moveTop();
-		} else if (command == moveBottomCommand) {
-			moveBottom();
-		} else {
-			uiController.commandAction(command, displayable);
-		}
-	}
+    public void commandAction(Command command, Displayable displayable) {
+        if (command == getMapCommand) {
+            uiController.getMapRequested();
+        } else if (command == moveUpCommand) {
+            moveUp();
+        } else if (command == moveDownCommand) {
+            moveDown();
+        } else if (command == moveTopCommand) {
+            moveTop();
+        } else if (command == moveBottomCommand) {
+            moveBottom();
+        } else {
+            uiController.commandAction(command, displayable);
+        }
+    }
 }
