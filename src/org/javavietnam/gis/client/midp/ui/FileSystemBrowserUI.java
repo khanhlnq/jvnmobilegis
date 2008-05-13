2f5ac8939ee8262696e718611f307a0eb9200445
phamvubinh123 <phamvubinh123@0cb8caaa-af32-0410-8db2-b50025d4c983>
2008-05-13 08:23:26 +0000

    Remarks: Issue 25. Fix java head.

--- src/org/javavietnam/gis/client/midp/ui/FileSystemBrowserUI.java
+++ src/org/javavietnam/gis/client/midp/ui/FileSystemBrowserUI.java
@@ -1,4 +1,41 @@
-// TODO Binh: Add Java file header
+/*
+ * $Id$
+ * $URL$
+ * $Author$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ * Copyright (C) 2006-2007 by JVNGIS
+ *
+ * All copyright notices regarding JVNMobileGIS MUST remain
+ * intact in the Java codes and resource files.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Support can be obtained from project homepage at:
+ * http://code.google.com/p/jvnmobilegis/
+ *
+ * Correspondence and Marketing Questions can be sent to:
+ * khanh.lnq AT gmail.com
+ *
+ * @version: 1.0
+ * @author: Khanh Le
+ * @Date Created: 22 Jun 2007
+ */
 
 package org.javavietnam.gis.client.midp.ui;
 
@@ -110,10 +147,14 @@ public class FileSystemBrowserUI extends List implements CommandListener {
 
 	public void commandAction(Command command, Displayable display) {
 		// TODO Binh: Should allowing browsing by pressing Action key (List.SELECT_COMMAND) 
+		if (List.SELECT_COMMAND == command) {
+			String label = getString(getSelectedIndex());
+			if (label.equals(uiController.getString(UIConstants.VIEW))) {
+				uiController.viewSaveToFileInputRequested();
+			}
+		}
 		if (command == save) {
 			uiController.viewSaveToFileInputRequested();
-		} else if (command == view) {
-			uiController.browseFileSystemRequested(display);
 		} else if (command == properties) {
 			uiController.viewPropertiesRequested(display);
 		} else if (command == back) {
