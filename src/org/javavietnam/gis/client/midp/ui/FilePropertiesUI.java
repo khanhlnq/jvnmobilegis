c0bd18aeac8f2db259c81f367468d6344dede6de
phamvubinh123 <phamvubinh123@0cb8caaa-af32-0410-8db2-b50025d4c983>
2008-05-16 11:47:36 +0000

    Remarks: Fix TODO list

--- src/org/javavietnam/gis/client/midp/ui/FilePropertiesUI.java
+++ src/org/javavietnam/gis/client/midp/ui/FilePropertiesUI.java
@@ -32,12 +32,10 @@
  * Correspondence and Marketing Questions can be sent to:
  * khanh.lnq AT gmail.com
  *
- * @version: 1.0
- * TODO Binh: Change author and date informations
- * @author: Khanh Le
- * @Date Created: 22 Jun 2007
+ * @version: 1.0.3
+ * @author: Binh Pham
+ * @Date Created: 09 May 2008
  */
-
 package org.javavietnam.gis.client.midp.ui;
 
 import java.io.IOException;
@@ -54,92 +52,91 @@ import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.StringItem;
 
-// TODO Binh: Format all source files using NetBeans Source/Format menu
 public class FilePropertiesUI extends Form implements CommandListener {
-	private UIController uiController;
-	private FileSystemBrowserUI fileSystemBrowserUI;
-	private FileConnection fileConnection;
-	private ChoiceGroup attrs;
-	private StringItem location, type, dateModified;
-	private Command back;
 
-	public static final String[] attrList = { "Read", "Write", "Hidden" };
+    private UIController uiController;
+    private FileSystemBrowserUI fileSystemBrowserUI;
+    private FileConnection fileConnection;
+    private ChoiceGroup attrs;
+    private StringItem location,  type,  dateModified;
+    private Command back;
+    public static final String[] attrList = {"Read", "Write", "Hidden"};
 
-	public FilePropertiesUI(UIController uiController,
-			FileSystemBrowserUI fileSystemBrowserUI) {
-		super("");
-		this.uiController = uiController;
-		this.fileSystemBrowserUI = fileSystemBrowserUI;
+    public FilePropertiesUI(UIController uiController,
+            FileSystemBrowserUI fileSystemBrowserUI) {
+        super("");
+        this.uiController = uiController;
+        this.fileSystemBrowserUI = fileSystemBrowserUI;
 
-		attrs = new ChoiceGroup("Attributes:", Choice.MULTIPLE, attrList, null);
+        attrs = new ChoiceGroup("Attributes:", Choice.MULTIPLE, attrList, null);
 
-		location = new StringItem("Location:", null);
-		type = new StringItem("Type: ", null);
-		dateModified = new StringItem("Modified:", null);
+        location = new StringItem("Location:", null);
+        type = new StringItem("Type: ", null);
+        dateModified = new StringItem("Modified:", null);
 
-		back = new Command(uiController.getString(UIConstants.BACK),
-				Command.BACK, 2);
-		
-		append(location);
-		append(type);
-		append(dateModified);
-		append(attrs);
+        back = new Command(uiController.getString(UIConstants.BACK),
+                Command.BACK, 2);
 
-		addCommand(back);
-		setCommandListener(this);
+        append(location);
+        append(type);
+        append(dateModified);
+        append(attrs);
 
-	}
+        addCommand(back);
+        setCommandListener(this);
 
-	public void getProperties(String fileName) throws IOException {
-		fileConnection = (FileConnection) Connector.open("file://localhost/"
-				+ fileSystemBrowserUI.getCurrPath() + fileName);
-		if (fileName.endsWith(FileSystemBrowserUI.UP_DIRECTORY)) {
-			return;
-		}
+    }
 
-		if (!fileConnection.exists()) {
-			throw new IOException("File does not exists");
-		}
+    public void getProperties(String fileName) throws IOException {
+        fileConnection = (FileConnection) Connector.open("file://localhost/" + fileSystemBrowserUI.getCurrPath() + fileName);
+        if (fileName.endsWith(FileSystemBrowserUI.UP_DIRECTORY)) {
+            return;
+        }
 
-		attrs.setSelectedFlags(new boolean[] { fileConnection.canRead(),
-				fileConnection.canWrite(), fileConnection.isHidden() });
-		
-		System.gc();
+        if (!fileConnection.exists()) {
+            throw new IOException("File does not exists");
+        }
 
-		location.setText(fileName);
-		type.setText(fileConnection.isDirectory() ? "Directory"
-				: "Regular File");
-		dateModified.setText(myDate(fileConnection.lastModified()));
+        attrs.setSelectedFlags(new boolean[]{fileConnection.canRead(),
+                    fileConnection.canWrite(), fileConnection.isHidden()
+                });
 
-		fileConnection.close();
-	}
+        System.gc();
 
-	private String myDate(long time) {
-		Calendar cal = Calendar.getInstance();
+        location.setText(fileName);
+        type.setText(fileConnection.isDirectory() ? "Directory"
+                : "Regular File");
+        dateModified.setText(myDate(fileConnection.lastModified()));
 
-		cal.setTime(new Date(time));
+        fileConnection.close();
+    }
 
-		StringBuffer sb = new StringBuffer();
+    private String myDate(long time) {
+        Calendar cal = Calendar.getInstance();
 
-		sb.append(cal.get(Calendar.HOUR_OF_DAY));
-		sb.append(':');
-		sb.append(cal.get(Calendar.MINUTE));
-		sb.append(':');
-		sb.append(cal.get(Calendar.SECOND));
-		sb.append(',');
-		sb.append(' ');
-		sb.append(cal.get(Calendar.DAY_OF_MONTH));
-		sb.append(' ');
-		sb.append(FileSystemBrowserUI.monthList[cal.get(Calendar.MONTH)]);
-		sb.append(' ');
-		sb.append(cal.get(Calendar.YEAR));
+        cal.setTime(new Date(time));
 
-		return sb.toString();
-	}
+        StringBuffer sb = new StringBuffer();
 
-	public void commandAction(Command command, Displayable display) {
-		if (command == back) {
-			uiController.viewFileSystemBrowserUIRequested();
-		}
-	}
+        sb.append(cal.get(Calendar.HOUR_OF_DAY));
+        sb.append(':');
+        sb.append(cal.get(Calendar.MINUTE));
+        sb.append(':');
+        sb.append(cal.get(Calendar.SECOND));
+        sb.append(',');
+        sb.append(' ');
+        sb.append(cal.get(Calendar.DAY_OF_MONTH));
+        sb.append(' ');
+        sb.append(FileSystemBrowserUI.monthList[cal.get(Calendar.MONTH)]);
+        sb.append(' ');
+        sb.append(cal.get(Calendar.YEAR));
+
+        return sb.toString();
+    }
+
+    public void commandAction(Command command, Displayable display) {
+        if (command == back) {
+            uiController.viewFileSystemBrowserUIRequested();
+        }
+    }
 }
--- src/org/javavietnam/gis/client/midp/ui/FileSystemBrowserUI.java
+++ src/org/javavietnam/gis/client/midp/ui/FileSystemBrowserUI.java
@@ -32,12 +32,10 @@
  * Correspondence and Marketing Questions can be sent to:
  * khanh.lnq AT gmail.com
  *
- * @version: 1.0
- * TODO Binh: Change author and date informations
- * @author: Khanh Le
- * @Date Created: 22 Jun 2007
+ * @version: 1.0.3
+ * @author: Binh Pham
+ * @Date Created: 09 May 2008
  */
-
 package org.javavietnam.gis.client.midp.ui;
 
 import java.io.IOException;
@@ -51,166 +49,160 @@ import javax.microedition.lcdui.*;
 import org.javavietnam.gis.client.midp.model.MessageCodes;
 import org.javavietnam.gis.shared.midp.ApplicationException;
 
-// TODO Binh: Format all source files using NetBeans Source/Format menu
 public class FileSystemBrowserUI extends List implements CommandListener {
 
-	public static final String[] attrList = { "Read", "Write", "Hidden" };
-	public static final String[] typeList = { "Regular File", "Directory" };
-	public static final String[] monthList = { "Jan", "Feb", "Mar", "Apr",
-			"May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+    public static final String[] attrList = {"Read", "Write", "Hidden"};
+    public static final String[] typeList = {"Regular File", "Directory"};
+    public static final String[] monthList = {"Jan", "Feb", "Mar", "Apr",
+        "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
 
-	/* special string denotes upper directory */
-	public static final String UP_DIRECTORY = "..";
+    /* special string denotes upper directory */
+    public static final String UP_DIRECTORY = "..";
 
-	/*
-	 * special string that denotes upper directory accessible by this browser.
-	 * this virtual directory contains all roots.
-	 */
-	public static final String MEGA_ROOT = "/";
+    /*
+     * special string that denotes upper directory accessible by this browser.
+     * this virtual directory contains all roots.
+     */
+    public static final String MEGA_ROOT = "/";
 
-	/* separator string as defined by FC specification */
-	public static final String SEP_STR = "/";
+    /* separator string as defined by FC specification */
+    public static final String SEP_STR = "/";
 
-	/* separator character as defined by FC specification */
-	public static final char SEP = '/';
+    /* separator character as defined by FC specification */
+    public static final char SEP = '/';
+    private UIController uiController;
+    private FileConnection fileConnection;
+    private String currPath;
+    private Image dirIcon;
+    private Image fileIcon;
+    private Enumeration enumeration;
+    private Command save;
+    private Command saveAs;
+    private Command view;
+    private Command properties;
+    private Command back;
 
-	private UIController uiController;
-	private FileConnection fileConnection;
-	private String currPath;
-	private Image dirIcon;
-	private Image fileIcon;
-	private Enumeration enumeration;
-	private Command save;
-	private Command saveAs;
-	private Command view;
-	private Command properties;
-	private Command back;
+    public FileSystemBrowserUI(UIController uiController) {
+        super(uiController.getString(UIConstants.FILE_SYSTEM), List.IMPLICIT);
+        this.uiController = uiController;
+        setCurrPath(MEGA_ROOT);
+        dirIcon = this.uiController.getImage(UIConstants.ICON_DIR);
+        fileIcon = this.uiController.getImage(UIConstants.ICON_FILE);
 
-	public FileSystemBrowserUI(UIController uiController) {
-		super(uiController.getString(UIConstants.FILE_SYSTEM), List.IMPLICIT);
-		this.uiController = uiController;
-		setCurrPath(MEGA_ROOT);
-		dirIcon = this.uiController.getImage(UIConstants.ICON_DIR);
-		fileIcon = this.uiController.getImage(UIConstants.ICON_FILE);
 
-                // TODO Binh: Hide View command for now. We already have SELECT_COMMAND
-		view = new Command(uiController.getString(UIConstants.VIEW),
-				Command.SCREEN, 1);
-		save = new Command(uiController.getString(UIConstants.SAVE),
-				Command.SCREEN, 2);
-		saveAs = new Command(uiController.getString(UIConstants.SAVE_AS),
-				Command.SCREEN, 3);
-		// TODO Binh: Hide Properties command for now
-                properties = new Command(
-				uiController.getString(UIConstants.PROPERTIES), Command.SCREEN,
-				4);
-		back = new Command(uiController.getString(UIConstants.BACK),
-				Command.BACK, 5);
-	}
+//        view = new Command(uiController.getString(UIConstants.VIEW),
+//                Command.SCREEN, 1);
+        save = new Command(uiController.getString(UIConstants.SAVE),
+                Command.SCREEN, 2);
+        saveAs = new Command(uiController.getString(UIConstants.SAVE_AS),
+                Command.SCREEN, 3);
 
-	public void getCurrDir() throws IOException {
-		if (MEGA_ROOT.equals(currPath)) {
-			enumeration = FileSystemRegistry.listRoots();
-			this.deleteAll();
-		} else {
-			fileConnection = (FileConnection) Connector
-					.open("file://localhost/" + currPath);
-			enumeration = fileConnection.list();
-			// not root - draw UP_DIRECTORY
-			this.deleteAll();
-			append(UP_DIRECTORY, dirIcon);
-		}
+//        properties = new Command(
+//                uiController.getString(UIConstants.PROPERTIES), Command.SCREEN,
+//                4);
+        back = new Command(uiController.getString(UIConstants.BACK),
+                Command.BACK, 5);
+    }
 
-		while (enumeration.hasMoreElements()) {
-			String fileName = (String) enumeration.nextElement();
+    public void getCurrDir() throws IOException {
+        if (MEGA_ROOT.equals(currPath)) {
+            enumeration = FileSystemRegistry.listRoots();
+            this.deleteAll();
+        } else {
+            fileConnection = (FileConnection) Connector.open("file://localhost/" + currPath);
+            enumeration = fileConnection.list();
+            // not root - draw UP_DIRECTORY
+            this.deleteAll();
+            append(UP_DIRECTORY, dirIcon);
+        }
 
-			if (fileName.charAt(fileName.length() - 1) == SEP) {
-				// This is directory
-				append(fileName, dirIcon);
-			} else {
-				// this is regular file
-				append(fileName, fileIcon);
-			}
-		}
+        while (enumeration.hasMoreElements()) {
+            String fileName = (String) enumeration.nextElement();
 
-		addCommand(view);
+            if (fileName.charAt(fileName.length() - 1) == SEP) {
+                // This is directory
+                append(fileName, dirIcon);
+            } else {
+                // this is regular file
+                append(fileName, fileIcon);
+            }
+        }
 
-		// Do not allow creating files/directories beside root
-		if (!currPath.equals(MEGA_ROOT)) {
-			addCommand(save);
-			addCommand(saveAs);
-			addCommand(properties);
-		} else {
-			removeCommand(save);
-			removeCommand(saveAs);
-			removeCommand(properties);
-		}
+        addCommand(view);
 
-		addCommand(back);
+        // Do not allow creating files/directories beside root
+        if (!currPath.equals(MEGA_ROOT)) {
+            addCommand(save);
+            addCommand(saveAs);
+            addCommand(properties);
+        } else {
+            removeCommand(save);
+            removeCommand(saveAs);
+            removeCommand(properties);
+        }
 
-		uiController.setCommands(this);
+        addCommand(back);
 
-		setCommandListener(this);
+        uiController.setCommands(this);
 
-		if (fileConnection != null) {
-			fileConnection.close();
-		}
-	}
+        setCommandListener(this);
 
-	public void commandAction(Command command, Displayable display) {
-		String label = getString(getSelectedIndex());
-		if (List.SELECT_COMMAND == command) {
-			if (label.endsWith(SEP_STR) || label.endsWith(UP_DIRECTORY)) {
-				uiController.browseFileSystemRequested(display);
-			} else {
-				uiController.saveMapToFileRequested(display);
-			}
-		}
-		if (command == view) {
-			uiController.browseFileSystemRequested(display);
-		} else if (command == save) {
-			if (label.endsWith(SEP_STR) || label.endsWith(UP_DIRECTORY)) {
-				uiController
-						.showErrorAlert(
-								new ApplicationException(
-										uiController
-												.getMessage(MessageCodes.ERROR_CAN_NOT_SAVE_MAP_TO_DIR)),
-								display);
-			} else {
-				uiController.saveMapToFileRequested(display);
-			}
-		} else if (command == saveAs) {
-			uiController.saveAsMapToFileRequested();
-		} else if (command == properties) {
-			if (label.endsWith(UP_DIRECTORY)) {
-				uiController
-						.showErrorAlert(
-								new ApplicationException(
-										uiController
-												.getMessage(MessageCodes.ERROR_CAN_NOT_VIEW_UP_DIR_PROPS)),
-								display);
-			} else {
-				uiController.viewPropertiesRequested(display);
-			}
-		} else if (command == back) {
-			uiController.viewMapRequested();
-		} else {
-			uiController.commandAction(command, display);
-		}
-	}
+        if (fileConnection != null) {
+            fileConnection.close();
+        }
+    }
 
-	/**
-	 * @return the currDirName
-	 */
-	public String getCurrPath() {
-		return currPath;
-	}
+    public void commandAction(Command command, Displayable display) {
+        String label = getString(getSelectedIndex());
+        if (List.SELECT_COMMAND == command) {
+            if (label.endsWith(SEP_STR) || label.endsWith(UP_DIRECTORY)) {
+                uiController.browseFileSystemRequested(display);
+            } else {
+                uiController.saveMapToFileRequested(display);
+            }
+        }
+        if (command == view) {
+            uiController.browseFileSystemRequested(display);
+        } else if (command == save) {
+            if (label.endsWith(SEP_STR) || label.endsWith(UP_DIRECTORY)) {
+                uiController.showErrorAlert(
+                        new ApplicationException(
+                        uiController.getMessage(MessageCodes.ERROR_CAN_NOT_SAVE_MAP_TO_DIR)),
+                        display);
+            } else {
+                uiController.saveMapToFileRequested(display);
+            }
+        } else if (command == saveAs) {
+            uiController.saveAsMapToFileRequested();
+        } else if (command == properties) {
+            if (label.endsWith(UP_DIRECTORY)) {
+                uiController.showErrorAlert(
+                        new ApplicationException(
+                        uiController.getMessage(MessageCodes.ERROR_CAN_NOT_VIEW_UP_DIR_PROPS)),
+                        display);
+            } else {
+                uiController.viewPropertiesRequested(display);
+            }
+        } else if (command == back) {
+            uiController.viewMapRequested();
+        } else {
+            uiController.commandAction(command, display);
+        }
+    }
 
-	/**
-	 * @param currDirName
-	 *            the currDirName to set
-	 */
-	public void setCurrPath(String currPath) {
-		this.currPath = currPath;
-	}
+    /**
+     * @return the currDirName
+     */
+    public String getCurrPath() {
+        return currPath;
+    }
+
+    /**
+     * @param currDirName
+     *            the currDirName to set
+     */
+    public void setCurrPath(String currPath) {
+        this.currPath = currPath;
+    }
 }
